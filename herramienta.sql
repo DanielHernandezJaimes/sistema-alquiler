b3cfa25342c073b1e562f633b8a5af63321ad8b1
Daniel Hernandez <dherjaimes@gmail.com>
2025-08-06 21:07:39 -0500

    se agregó el precio_alquiler

--- herramienta.sql
+++ herramienta.sql
@@ -1,5 +1,6 @@
 CREATE TABLE herramientas(
     id_herramienta INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_herramienta VARCHAR (100)
-    
+    nombre_herramienta VARCHAR (100),
+    precio_alquiler INT
 );
+
